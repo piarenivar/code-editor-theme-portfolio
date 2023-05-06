af3e2f5d9298749d97021ae53d42332c54466d54
Pablo Arenivar <pabloarenivar@Pablos-MacBook-Pro.local>
2023-05-05 20:46:58 -0500

    Update Contact page. Finished backend logic for contact form.

---
+++ src/components/global/Loader.jsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+    return (
+        <svg viewBox='25 25 50 50' className='loader'>
+            <circle r='20' cy='50' cx='50' className='loader-inner' />
+        </svg>
+    )
+}
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,36 @@
 import styles from '../styles/modules/Pages.module.css';
+
 import Link from "next/link"
+import { useState } from 'react';
+
+import Loader from '@/components/global/Loader';
+
+import { sendContactForm } from '@/lib/api';
+
+const initValues = { name: '', email: '', subject: '', message: '' };
+const initState = { isLoading: false, values: initValues };
 
 export default function ContactPage() {
+  const [state, setState] = useState(initState);
+  const { isLoading, values } = state;
+
+  const handleChange = ({ target }) => setState((prev) => ({
+    ...prev,
+    values: {
+      ...prev.values,
+      [target.name]: target.value,
+    }
+  }))
+
+  const onSubmit = async () => {
+    setState((prev) => ({
+      ...prev,
+      isLoading: true
+    }));
+
+    await sendContactForm(values)
+  }
+
   return (
     <main className={styles.main}>
       <h2 className={styles.title}>Get In Touch</h2>
@@ -12,12 +41,12 @@ export default function ContactPage() {
           <Link href='https://www.linkedin.com/in/pabloarenivar/' className={`center-y underline-link ${styles.contact_link}`} target='_blank'><i className='center fa-brands fa-linkedin' />linkedin</Link>
           <Link href='https://codepen.io/piarenivar' className={`center-y underline-link ${styles.contact_link}`} target='_blank'><i className='center fa-brands fa-codepen' />codepen</Link>
         </div>
-        <form className={styles.contact_form}>
-          <input placeholder='Name' className={styles.contact_name}></input>
-          <input placeholder='Email' className={styles.contact_email}></input>
-          <input placeholder='Subject' className={styles.contact_subject}></input>
-          <textarea className={styles.contact_textarea} />
-          <button type='submit' className={`center ${styles.contact_submit}`}>Submit</button>
+        <form className={styles.contact_form} required>
+          <input type='text' name='name' value={values.name} onChange={handleChange} placeholder='Name' className={styles.contact_name} required />
+          <input type='email' name='email' value={values.email} onChange={handleChange} placeholder='Email' className={styles.contact_email} required />
+          <input type='text' name='subject' value={values.subject} onChange={handleChange} placeholder='Subject' className={styles.contact_subject} required />
+          <textarea type='text' name='message' value={state.message} onChange={handleChange} placeholder='Message' className={styles.contact_textarea} required />
+          <button type='submit' disabled={!values.name || !values.email || !values.subject || !values.message} onClick={onSubmit} isLoading={isLoading} className={`center ${styles.contact_submit}`}>{isLoading ? <Loader /> : 'Submit'}</button>
         </form>
       </div>
     </main>
